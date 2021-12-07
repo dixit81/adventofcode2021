d64a0444974ac492b71350f0f106ed02b5275a68
Devashish Dixit <deva_0801@hotmail.com>
2021-12-07 21:05:21 +0000

    Day 1, part 2

--- src/main/java/Day1.java
+++ src/main/java/Day1.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day1 {
@@ -22,5 +23,27 @@ public class Day1 {
             return count;
         }
 
+    }
+
+    public int part2() {
+        if(input.size() <= 3) {
+            return 0;
+        } else {
+            final List<Integer> sums = new ArrayList<>();
+            int count = 0;
+            int sum = 0;
+
+            for(int i=0; i<input.size()-2; i++) {
+                sum = input.get(i) + input.get(i+1) + input.get(i+2);
+                sums.add(sum);
+            }
+
+            for(int i=0; i<sums.size()-1; i++) {
+                if(sums.get(i+1) > sums.get(i)) {
+                    count++;
+                }
+            }
+            return count;
+        }
     }
 }
--- src/test/java/Day1Test.java
+++ src/test/java/Day1Test.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
@@ -13,50 +14,122 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day1Test {
 
-    @Test
-    void givenNoDepthThereIsNoIncreaseInDepth() {
-        final List<Integer> input = Collections.emptyList();
-        final Day1 day1 = new Day1(input);
+    @Nested
+    class part1 {
+        @Test
+        void givenNoDepthThereIsNoIncreaseInDepth() {
+            final List<Integer> input = Collections.emptyList();
+            final Day1 day1 = new Day1(input);
 
-        assertEquals(0, day1.part1());
+            assertEquals(0, day1.part1());
+        }
+
+        @Test
+        void givenADepthThereIsNoIncreaseInDepth() {
+            final List<Integer> input = Collections.singletonList(100);
+            final Day1 day1 = new Day1(input);
+
+            assertEquals(0, day1.part1());
+        }
+
+        @Test
+        void givenTwoDepthsInIncreasingOrderItReturnsOneIncreasingDepth() {
+            final List<Integer> input = new ArrayList<>();
+            input.add(100);
+            input.add(101);
+
+            final Day1 day1 = new Day1(input);
+            assertEquals(1, day1.part1());
+        }
+
+        @Test
+        void givenThreeDepthsInIncreasingOrderItReturnsTwoInIncreasingDepth() {
+            final List<Integer> input = new ArrayList<>();
+            input.add(100);
+            input.add(101);
+            input.add(102);
+
+            final Day1 day1 = new Day1(input);
+            assertEquals(2, day1.part1());
+        }
+
+        @Test
+        void givenTestInputWithRandomNumbersItShouldReturnAValue() throws IOException {
+
+            final BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/testfiles/day1.txt")));
+            final List<Integer> input = reader.lines().map(Integer::parseInt).collect(Collectors.toList());
+            final Day1 day1 = new Day1(input);
+
+            assertEquals(1766, day1.part1());
+        }
     }
 
-    @Test
-    void givenADepthThereIsNoIncreaseInDepth() {
-        final List<Integer> input = Collections.singletonList(100);
-        final Day1 day1 = new Day1(input);
+    @Nested
+    class part2 {
+        @Test
+        void givenThreeMeasurementsItShouldReturnZero() {
+            final List<Integer> input = new ArrayList<>();
+            input.add(100);
+            input.add(101);
+            input.add(102);
 
-        assertEquals(0, day1.part1());
+            final Day1 day1 = new Day1(input);
+            assertEquals(0, day1.part2());
+        }
+
+        @Test
+        void givenFourMeasurementsItShouldReturnOne() {
+            final List<Integer> input = new ArrayList<>();
+            input.add(100);
+            input.add(101);
+            input.add(102);
+            input.add(103);
+
+            final Day1 day1 = new Day1(input);
+            assertEquals(1, day1.part2());
+        }
+
+        @Test
+        void givenFiveMeasurementsItShouldReturnTwo() {
+            final List<Integer> input = new ArrayList<>();
+            input.add(100);
+            input.add(101);
+            input.add(102);
+            input.add(103);
+            input.add(104);
+
+            final Day1 day1 = new Day1(input);
+            assertEquals(2, day1.part2());
+        }
+
+        @Test
+        void givenTestExampleAndItShouldReturnFive() {
+            final ArrayList<Integer> input = new ArrayList<>();
+            input.add(199);
+            input.add(200);
+            input.add(208);
+            input.add(210);
+            input.add(200);
+            input.add(207);
+            input.add(240);
+            input.add(269);
+            input.add(260);
+            input.add(263);
+
+            final Day1 day1 = new Day1(input);
+            assertEquals(5, day1.part2());
+        }
+
+        @Test
+        void givenTestInputWithRandomNumbersItShouldReturnAValue() throws IOException {
+
+            final BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/testfiles/day1.txt")));
+            final List<Integer> input = reader.lines().map(Integer::parseInt).collect(Collectors.toList());
+            final Day1 day1 = new Day1(input);
+
+            assertEquals(1797, day1.part2());
+        }
+
     }
 
-    @Test
-    void givenTwoDepthsInIncreasingOrderItReturnsOneIncreasingDepth() {
-        final List<Integer> input = new ArrayList<>();
-        input.add(100);
-        input.add(101);
-
-        final Day1 day1 = new Day1(input);
-        assertEquals(1, day1.part1());
-    }
-
-    @Test
-    void givenThreeDepthsInIncreasingOrderItReturnsTwoInIncreasingDepth() {
-        final List<Integer> input = new ArrayList<>();
-        input.add(100);
-        input.add(101);
-        input.add(102);
-
-        final Day1 day1 = new Day1(input);
-        assertEquals(2, day1.part1());
-    }
-
-    @Test
-    void givenTestInputWithRandomNumbersItShouldReturnAValue() throws IOException {
-
-        final BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/testfiles/day1.txt")));
-        final List<Integer> input = reader.lines().map(Integer::parseInt).collect(Collectors.toList());
-        final Day1 day1 = new Day1(input);
-
-        assertEquals(1766, day1.part1());
-    }
 }
